60a6e7bebfe9c8b9ce535b7549e55f0627aa3767
Iryna Dobrovolska <dobryk@google.com>
2020-09-09 17:31:36 +0000

    send jsonnet program as command line argument
    for now it is possible to pass only one parameter
    but it can be easily changed

--- core/type_inference/print_ast.cpp
+++ core/type_inference/print_ast.cpp
@@ -8,7 +8,6 @@
 #include "lexer.h"
 #include "parser.h"
 
-const bool test_mode = false;
 
 std::ostream &operator<<(std::ostream &out, const LocationRange *lr);
 
@@ -19,12 +18,12 @@ std::ostream &operator<<(std::ostream &out, const ArgParam arg_param)
     out << "ast.ArgParam(";
     if (arg_param.id) {
         out << "id=\"";
-        out << arg_param.id;  // can be nullptr
+        out << arg_param.id;  
         out << "\", ";
     }
     if (arg_param.expr) {
         out << "expr=";
-        out << arg_param.expr;  // can be nullptr
+        out << arg_param.expr; 
     }
     out << ")";
     return out;
@@ -33,7 +32,7 @@ std::ostream &operator<<(std::ostream &out, const ArgParam arg_param)
 std::ostream &operator<<(std::ostream &out, const Local::Bind& bind)
 {
     UString std = decode_utf8("std");
-    if (bind.var->name == std) {// TODO: decide what to do with std-bind
+    if (bind.var->name == std) {
         out << "ast.Bind(\"std\", ast.LiteralNull())";
         return out;
     }
@@ -47,7 +46,6 @@ std::ostream &operator<<(std::ostream &out, const Local::Bind& bind)
     return out;
 }
 
-// Overloading '<<' for AST and its nodes
 std::ostream &operator<<(std::ostream &out, const Apply *ast)
 {
     out << "ast.Apply(";
@@ -206,7 +204,7 @@ std::ostream &operator<<(std::ostream &out, const LiteralString *ast)
 std::ostream &operator<<(std::ostream &out, const DesugaredObject *ast)
 {
     // - maybe it is better to overload operator << for Field structure
-    // - maybe we will need hide field to kepp info about inheritance
+    // - maybe we will need hide field to keep info about inheritance
     out << "ast.Object(";
     out << &ast->location;
     out << ",";
@@ -397,280 +395,22 @@ std::ostream &operator<<(std::ostream &out, const AST *ast_)
     return out;
 }
 
-void print_tokens(Tokens tokens)
-{
-    std::cout << "Tokens:\n";
-    for (auto t : tokens) {
-        std::cout << t << ", ";
-    }
-    std::cout << std::endl;
-}
-
-void write_to_file(std::string filename, AST *ast)
-{
-    std::ofstream myfile;
-    myfile.open(filename);
-    myfile << ast;
-    myfile.close();
-}
-
-const char *examples(int example)
-{
-    const char *res = "";
-    switch (example) {
-        case 1: res = "if 2 == 6 then \"magic\" else 0"; break;
-        case 2: res = "[1, 2, 3]"; break;
-        case 3:
-            res = R""""({ 
-                person1: {
-                    name: "Alice",
-                    welcome: "Hello " + self.name + "!",
-                },
-                person2: self.person1 { name: "Bob" },
-            })"""";
-            break;
-        case 4: res = "{local b = [1, 2, 3, 4, 5], b: b[1::2], s: self.b}"; break;
-        case 5:
-            res = R""""(
-                local person(name) = {
-                    name: name,
-                    welcome: 'Hello ' + name + 3,
-                }; 
-                {}
-            )"""";
-            break;
-        case 6:  // example that produces Index node
-            res = R""""({ 
-                local obj = self,
-                person1: {
-                    name: "Alice",
-                    kind: "child",
-                },
-                person2: { 
-                    name: "Bob",
-                    kind: obj.person1.kind 
-                },
-            })"""";
-            break;
-        case 7:  // example that produces SuperIndex and InSuper nodes
-            res = R""""({ 
-                local person = {
-                    name: "Alice",
-                    age: 20,
-                    country: "Wonderland"
-                },
-                student: person + { 
-                    name: super.name,
-                    age: super["age"],  
-                    university: "MIT", 
-                    has_country: "country" in super, 
-                },
-            })"""";
-            break;
-        case 8:  // example of array comprehension
-            res = R""""({ 
-                local person = {
-                    friends: ["Martin", "Lu"]
-                },
-                student: person + { 
-                    university: "MIT", 
-                    contacts: [i + "_" + self.university for i in super.friends]  
-                },
-            })"""";
-            break;
-        case 9: res = "{b: 3, assert b > 2 : 'b must be bigger than 2'}"; break;
-        case 10:  // text block will be printed in AST as multiline text
-            res = R""""(
-                {
-                    text: |||  
-                        text
-                        block
-                    |||
-                }
-            )"""";
-        case 11:
-            res = R""""(
-                {
-                    local uni = "TUM",
-                    age: 5,
-                    year: 2020,
-                    c: {
-                        local sem = 3,
-                        age: $['age'],
-                        x: {
-                            x_year: $['year'],
-                            x_sem: sem,
-                            x_obj: {
-                                y: y_smth
-                            }
-                        }
-                    }
-                }
-            )"""";
-            break;
-        case 12:
-            res = R""""(
-                {
-                    a: {
-                        local var = $["b"],
-                        c: var 
-                    },
-                    b: 2
-                }
-            )"""";
-            break;
-        case 13:
-            res = R""""(
-                {
-                    a: 2,
-                    b: self.a,
-                }
-            )"""";
-            break;    
-        case 14:
-            res = R""""( 
-                {
-                    a: "lala",
-                    person1: { 'name': 1},
-                }
-            )"""";
-            break; 
-        case 15:
-            res = R""""( 
-                {
-                    a: self.b,
-                    b: self.a,
-                }
-            )"""";
-            break;
-        case 16:
-            res = R""""( 
-                {
-                    local l = a, 
-                    local a = self.b, 
-                    b: 2, 
-                    c: l 
-                }
-            )"""";
-            break; 
-        case 17:
-            res = R""""( 
-                {
-                    local a = 'yes',
-                    b: {
-                        c: {
-                            a: 5,
-                            d: a,
-                        },
-                        k: {
-                            d: a,
-                        }
-                    }
-                }
-            )"""";
-            break; 
-        case 18:
-            res = R""""(
-                {
-                    local a = 'l',
-                    local func(a) = {
-                        x: {
-                            field1: a,
-                        }
-                    },
-                    k: func(4),
-                    l: func('Google')
-                }
-            )"""";   
-            break;
-        case 19:
-            res = R""""(
-                {
-                    local person = {
-                        name: 'lala',
-                    },
-                    student: person { 
-                        local coef = 1,
-                        name: 'Ali', 
-                        b: coef  
-                    },
-                }
-            )"""";   
-            break;
-        case 20:
-            res = R""""(
-                {
-                    a: 1,
-                    b: self.a + 2,
-                    c: self.a + self.b
-                }
-            )"""";   
-            break; 
-        case 21:
-            res = R""""(
-                {
-                    person: {
-                        name: "No name",
-                    },
-                    student: self.person { 
-                        local coef = 1,
-                        name: 'Ali', 
-                        b: coef  
-                    },
-                }
-            )"""";   
-            break;
-        case 22:
-            res = R""""(
-                {
-                    local person = {
-                        name: 0,
-                    },
-                    student: person { 
-                        name: 'Ali', 
-                        age: 19  
-                    },
-                }
-            )"""";   
-            break;
-        case 23:
-            res = R""""(
-                {
-                    local person = {
-                        name: 'no name',
-                    },
-                    student: person { 
-                        name: 'Ali', 
-                        age: 19,
-                        best_friend: person {
-                            age: 18,
-                            has_friend: true
-                        }  
-                    },
-                }
-            )"""";   
-            break;                
-        default: break;
-    }
-    return res;
-}
-
 int main(int argc, char const *argv[])
 {
-    const char *input = examples(23);
-    Allocator *alloc = new Allocator();
+    const char *input;
+    if (argc < 2) {
+        std::cerr << "No jsonnet example to execute " << std::endl;
+    } else if (argc > 2) {
+        std::cerr << "Too many cmd arguments " << std::endl;
+    } else {
+        input = argv[1];
+    }
 
+    Allocator *alloc = new Allocator();
     Tokens tokens = jsonnet_lex("", input);
     AST *ast = jsonnet_parse(alloc, tokens);
     jsonnet_desugar(alloc, ast, nullptr);
-
-    if (test_mode) {
-        std::cout << "AST nodes:\n;";
-        std::cout << ast;
-        std::cout << std::endl;
-    }
-    // TODO: pass the name of file as commmand line arg
-    write_to_file("core/type_inference/ast_string.txt", ast);
-
+    
+    std::cout << ast;
     return 0;
 }
