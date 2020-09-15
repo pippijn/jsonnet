5a374a2e141a6c07b4b7c4c8703b16ac57c08d19
Iryna Dobrovolska <dobryk@google.com>
2020-09-15 15:12:27 +0000

    add aliases for imported ast modules

--- core/type_inference/translate_jsonnet_to_lambda.py
+++ core/type_inference/translate_jsonnet_to_lambda.py
@@ -1,10 +1,10 @@
-import jsonnet_ast as ast
-from lambda_ast import *
+import jsonnet_ast as j_ast
+import lambda_ast as lam_ast
 from lambda_types import TypeVariable, TypeRowOperator, Function, TypeOperator
 
 
-def translate_to_lambda_ast(ast_: ast.AST, my_env):
-    if isinstance(ast_, ast.Object):
+def translate_to_lambda_ast(ast_: j_ast.AST, my_env):
+    if isinstance(ast_, j_ast.Object):
         ast_.fields = {translate_field_name(
             name): val for name, val in ast_.fields.items()}
         record = build_record_type_constructor(ast_.fields)
@@ -15,86 +15,86 @@ def translate_to_lambda_ast(ast_: ast.AST, my_env):
         body = apply_record(field_names, record_id, my_env)
         return build_letrec_and(field_names, ast_.fields, body, my_env)
 
-    elif isinstance(ast_, ast.Local):
+    elif isinstance(ast_, j_ast.Local):
         bind_dic = {}
         for bind in ast_.binds:
             translated_body = translate_to_lambda_ast(bind.body, my_env)
             bind_dic[bind.var] = translated_body
         body = translate_to_lambda_ast(ast_.body, my_env)
-        if isinstance(body, LetrecAnd):
+        if isinstance(body, lam_ast.LetrecAnd):
             body.bindings.update(bind_dic)
             return body
         else:
-            return LetrecAnd(bind_dic, body)
+            return lam_ast.LetrecAnd(bind_dic, body)
 
-    elif isinstance(ast_, ast.Apply):
+    elif isinstance(ast_, j_ast.Apply):
         def build_apply(fn, args):
             if not args:
                 return translate_to_lambda_ast(fn, my_env)
             translated_arg = translate_to_lambda_ast(args[-1].expr, my_env)
-            return Apply(build_apply(fn, args[:-1]), translated_arg)
+            return lam_ast.Apply(build_apply(fn, args[:-1]), translated_arg)
         return build_apply(ast_.fn, ast_.arguments)
 
-    elif isinstance(ast_, ast.Array):
+    elif isinstance(ast_, j_ast.Array):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.BinaryOp):
+    elif isinstance(ast_, j_ast.BinaryOp):
         if ast_.op == '+':
             return build_plus_op(ast_.left_arg, ast_.right_arg, my_env)
         else:
             raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.BuiltinFunction):
+    elif isinstance(ast_, j_ast.BuiltinFunction):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Conditional):
+    elif isinstance(ast_, j_ast.Conditional):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Error):
+    elif isinstance(ast_, j_ast.Error):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Function):
+    elif isinstance(ast_, j_ast.Function):
         def build_lambda(args, body):
             if not args:
                 return translate_to_lambda_ast(body, my_env)
-            return Lambda(args[0].id, build_lambda(args[1:], body))
+            return lam_ast.Lambda(args[0].id, build_lambda(args[1:], body))
         return build_lambda(ast_.arguments, ast_.body)
 
-    elif isinstance(ast_, ast.InSuper):
+    elif isinstance(ast_, j_ast.InSuper):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Index):
-        if isinstance(ast_.target, ast.Self) and isinstance(ast_.index, ast.LiteralString):
-            return Identifier(ast_.index.value)
+    elif isinstance(ast_, j_ast.Index):
+        if isinstance(ast_.target, j_ast.Self) and isinstance(ast_.index, j_ast.LiteralString):
+            return lam_ast.Identifier(ast_.index.value)
         else:
             raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.LiteralBoolean):
-        return LiteralBoolean(ast_.value)
+    elif isinstance(ast_, j_ast.LiteralBoolean):
+        return lam_ast.LiteralBoolean(ast_.value)
 
-    elif isinstance(ast_, ast.LiteralNumber):
-        return LiteralNumber(ast_.value)
+    elif isinstance(ast_, j_ast.LiteralNumber):
+        return lam_ast.LiteralNumber(ast_.value)
 
-    elif isinstance(ast_, ast.LiteralString):
-        return LiteralString(ast_.value)
+    elif isinstance(ast_, j_ast.LiteralString):
+        return lam_ast.LiteralString(ast_.value)
 
-    elif isinstance(ast_, ast.LiteralNull):
-        return Identifier("null")
+    elif isinstance(ast_, j_ast.LiteralNull):
+        return lam_ast.Identifier("null")
 
-    elif isinstance(ast_, ast.ObjectComprehensionSimple):
+    elif isinstance(ast_, j_ast.ObjectComprehensionSimple):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Self):
-        return Identifier("self")
+    elif isinstance(ast_, j_ast.Self):
+        return lam_ast.Identifier("self")
 
-    elif isinstance(ast_, ast.SuperIndex):
-        return Identifier(ast_.index.value)
+    elif isinstance(ast_, j_ast.SuperIndex):
+        return lam_ast.Identifier(ast_.index.value)
 
-    elif isinstance(ast_, ast.UnaryOp):
+    elif isinstance(ast_, j_ast.UnaryOp):
         raise Exception('Not translated yet!\n')
 
-    elif isinstance(ast_, ast.Var):
-        return Identifier(ast_.id)
+    elif isinstance(ast_, j_ast.Var):
+        return lam_ast.Identifier(ast_.id)
 
     else:
         print(ast_.__class__)
@@ -126,14 +126,14 @@ def build_letrec_and(names, fields, body, env):
     for name in names:
         translated_body = translate_to_lambda_ast(fields[name], env)
         translated_fields[name] = translated_body
-    return LetrecAnd(translated_fields, body)
+    return lam_ast.LetrecAnd(translated_fields, body)
 
 
 def build_let(names, fields, body, env):
     if not names:
         return body
     translated_body = translate_to_lambda_ast(fields[names[0]], env)
-    return Let(names[0], translated_body, build_let(names[1:], fields, body, env))
+    return lam_ast.Let(names[0], translated_body, build_let(names[1:], fields, body, env))
 
 
 def get_next_record_id(env):
@@ -150,26 +150,26 @@ def get_next_plus_id(env):
 
 def apply_record(names, record_id, env):
     if not names:
-        return Identifier(record_id)
-    return Apply(apply_record(names[:-1], record_id, env), Identifier(names[-1]))
+        return lam_ast.Identifier(record_id)
+    return lam_ast.Apply(apply_record(names[:-1], record_id, env), lam_ast.Identifier(names[-1]))
 
 
 def translate_field_name(name):
-    if isinstance(name, ast.LiteralString):
+    if isinstance(name, j_ast.LiteralString):
         return name.value
     else:
         raise Exception(f"Expected LiteralString but got {name.__class__}")
 
 
 def build_plus_op(left_arg, right_arg, env):
-    if isinstance(right_arg, ast.Object):
+    if isinstance(right_arg, j_ast.Object):
         base_obj = translate_to_lambda_ast(left_arg, env)
         child_obj = translate_to_lambda_ast(right_arg, env)
-        return Inherit(base_obj, child_obj)
+        return lam_ast.Inherit(base_obj, child_obj)
     else:
         var = TypeVariable()
         plus_id = get_next_plus_id(env)
         env[plus_id] = Function(var, Function(var, var))
-        return Apply(Apply(Identifier(plus_id),
+        return lam_ast.Apply(lam_ast.Apply(lam_ast.Identifier(plus_id),
                            translate_to_lambda_ast(left_arg, env)),
                      translate_to_lambda_ast(right_arg, env))
