498e53e443b514baea81f1b506297e6573e0ee8a
Iryna Dobrovolska <dobryk@google.com>
2020-10-06 07:55:07 +0000

    fix expexted type of test due to changes in algo

--- core/type_inference/parametrized_test.py
+++ core/type_inference/parametrized_test.py
@@ -1,10 +1,15 @@
 import pytest
 import infer
 
+""" Module with parametrized integration tests.
+To run tests from jsonnet folder, execute the following: 
+pytest core/type_inference/parametrized_test.py 
+"""
+
 testdata = [
     (1, 2, '{x: number, y: number, sum: number}'),
     ('"lala"', '"lalala"', '{x: string, y: string, sum: string}'),
-    (1, '"la"', 'Type mismatch: string != number')    
+    (1, '"la"', 'Type mismatch: string != number, line 1')
 ]
 
 
